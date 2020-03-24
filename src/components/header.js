67d71f5dfe4663b18fd2e709e95a0124ae5201be
Martijn <mbankert145@gmail.com>
2020-03-24 14:12:04 +0100

    Update colors

--- src/components/header.js
+++ src/components/header.js
@@ -4,6 +4,8 @@ const Header = () => {
   return (
     <header className="header">
         <div className="header__image" style={{ backgroundImage: `url(${bicycleHeader})` }}>
+          <div className="header__overlay"></div>
+          <h1 className="header__title">L'escargo de la Liberté</h1>
         </div>
     </header>
   );
