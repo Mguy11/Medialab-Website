d8ce2b988e5234369e152b545716fd3aa2bea60c
Martijn <mbankert145@gmail.com>
2020-03-24 20:45:38 +0100

    Header update

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
-import bicycleHeader from '../images/bicycle-orange-wheels.jpg';
+import HeaderImage from '../images/bevrijdingsfestival.jpg';
+import logo from '../images/Medialab-logo-trans.png';
 const Header = () => {
   return (
-    <header className="header">
-        <div className="header__image" style={{ backgroundImage: `url(${bicycleHeader})` }}>
-          <div className="header__overlay"></div>
+    <header className="header" style={{ backgroundImage: `url(${HeaderImage})` }}>
+        <div className="header__content">
+          <img src="../images/Medialab-logo-trans.png" className="header__logo" alt="logo"></img>
           <h1 className="header__title">L'escargo de la Liberté</h1>
         </div>
     </header>
