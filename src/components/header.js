f37438ee2dee1a825717d57b4ee587a108fb9d86
Martijn <mbankert145@gmail.com>
2020-03-26 16:46:34 +0100

    update

--- src/components/header.js
+++ src/components/header.js
@@ -4,10 +4,11 @@ import logo from '../images/Medialab-logo-trans.png';
 const Header = () => {
   return (
     <header className="header" style={{ backgroundImage: `url(${HeaderImage})` }}>
+      <div className="wrap">
         <div className="header__content">
-          <img src="../images/Medialab-logo-trans.png" className="header__logo" alt="logo"></img>
           <h1 className="header__title">L'escargo de la Liberté</h1>
         </div>
+      </div>
     </header>
   );
 }
