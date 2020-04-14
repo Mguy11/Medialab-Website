9e162c609e4a2e7030bf840068254bcef7be321c
Martijn <mbankert145@gmail.com>
2020-04-14 18:22:16 +0200

    reversed oreder

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import Header from './components/header';
 import './styles/style.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/header';
 import Team from './components/team';
 import Research from './components/research';
 import Insights from './components/insights';
